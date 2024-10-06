27702e28445d6ea98fec09d15c0c23df8f023f3e
Fabiano <fbnfdcs@gmail.com>
2024-10-05 21:46:48 -0300

    05/10/2024

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,18 @@
 import 'package:flutter/material.dart';
 
+//import 'dart:io';
+
+import 'package:sqlite3/sqlite3.dart';
+
 void main() {
   runApp(const MainApp());
+
+  print('Using sqlite3 ${sqlite3.version}');
+
+  final db = sqlite3.open(taskify.db);
+
+  db.execute(''' CREATE TABLE users (id INTEGER NOT NULL PRIMARY KEY, name TEXT NOT NULL);  ''');
+
 }
 
 class MainApp extends StatelessWidget {
