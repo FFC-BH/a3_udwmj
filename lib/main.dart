08199fac151370f9ecf446f62a40f074fa9256fa
Fabiano <fbnfdcs@gmail.com>
2024-10-06 01:29:17 -0300

    06/10/2024

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,49 @@
+import 'package:a3_udwmj/view/home.dart';
 import 'package:flutter/material.dart';
 
-//import 'dart:io';
+import 'package:sqlite3/sqlite3.dart' show sqlite3;
 
-import 'package:sqlite3/sqlite3.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MainApp());
 
-  print('Using sqlite3 ${sqlite3.version}');
+ // print('Using sqlite3 ${sqlite3.version}');
 
-  final db = sqlite3.open(taskify.db);
+  final db = sqlite3.open('taskify.db');
 
-  db.execute(''' CREATE TABLE users (id INTEGER NOT NULL PRIMARY KEY, name TEXT NOT NULL);  ''');
+  db.execute(''' CREATE TABLE IF NOT EXISTS users 
+                    (id_user INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                     nome VARCHAR NOT NULL, 
+                     email VARCHAR NOT NULL, 
+                     senha VARCHAR NOT NULL
+                     );''');  
+
+
+  db.execute(''' CREATE TABLE IF NOT EXISTS tasks
+                    (id_tsk INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                     id_user INTEGER NOT NULL,
+                     status_tsk INTEGER NOT NULL,
+                     nome_tsk VARCHAR NOT NULL,
+                     descricao_tsk TEXT NOT NULL,
+                     dt_inicio VARCHAR NOT NULL,
+                     dt_fim VARCHAR NOT NULL,
+                     CONSTRAINT fk_users
+                        FOREING KEY (id_user)
+                        REFERENCES users(id_user)
+                        ON DELETE CASCADE
+                        ON UPDATE NO ACTION
+                    );''');
+                   
 
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  //const MainApp({super.key});
+  get routes => <String, WidgetBuilder>{
+   // LoginPage.tag: (context) => LoginPage(),
+    Home.tag: (context) => Home()
+  };
+
 
   @override
   Widget build(BuildContext context) {
---
+++ lib/view/home.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+
+
+class Home extends StatelessWidget {
+  static String tag = 'home';
+  
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+
+}
